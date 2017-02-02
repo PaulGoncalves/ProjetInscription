faa02b8669997aaf8dbdc68d7edf6d777ff9a156
PaulGoncalves <paul.goncalves@ynov.com>
2017-02-02 17:28:33 +0100

    Connexion Bdd

--- src/com/objis/BaseDeDonneeJdbc/BaseDeDonneeJdbc.java
+++ src/com/objis/BaseDeDonneeJdbc/BaseDeDonneeJdbc.java
@@ -1,10 +1,24 @@
 package com.objis.BaseDeDonneeJdbc;
 
-public class BaseDeDonneeJdbc {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.sql.*;
+public class BaseDeDonneeJdbc 
+{
+	public static void main (String[] args)
+	{
+		try{
+			//CONNECTION TO DATABASE
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/inscriptionSportive", "root" , "");
+			//CREATON DU STATEMENT
+			Statement myStmt = myConn.createStatement();
+			//EXECUTER SQL QUERY
+			ResultSet myRs = myStmt.executeQuery("SELECT * FROM personne");
+				while (myRs.next())
+				{
+						System.out.println(myRs.getString("prenom_personne") + ", " + myRs.getString("mail"));
+				}
+			}
+		catch (Exception exc){
+			exc.printStackTrace();
+		}
 	}
-
 }

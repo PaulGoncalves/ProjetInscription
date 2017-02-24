4ad86e2e4fdd2943a1ac33edd834f9815bc99a7f
PaulGoncalves <paul.goncalves@ynov.com>
2017-02-24 10:23:37 +0100

    Modif connexion Bdd

--- src/com/objis/BaseDeDonneeJdbc/BaseDeDonneeJdbc.java
+++ src/com/objis/BaseDeDonneeJdbc/BaseDeDonneeJdbc.java
@@ -6,8 +6,13 @@ public class BaseDeDonneeJdbc
 	public static void main (String[] args)
 	{
 		try{
+			//Chargement du dirver
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/inscriptionsportive";
+			String login = "root";
+			String mdp = "";
 			//Connexion à la base de donnée
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/inscriptionSportive", "root" , "");
+			Connection myConn = DriverManager.getConnection(url, login , mdp);
 			//CREATON DU STATEMENT
 			Statement stmt = myConn.createStatement();
 			//srcipt sql recuperation resultat
